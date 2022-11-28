9d6e12d2d86a5c9a28d44b2c6f78aebb8e7d55bb
HeesooSong <cleos0409@gmail.com>
2022-11-28 18:27:21 +0900

    updated

--- set_to_merge_test.py
+++ set_to_merge_test.py
@@ -7,12 +7,15 @@ Created on Fri Nov 25 14:46:49 2022
 
 import pandas as pd
 import math
+import numpy
 
 def eucledean (x1, x2, y1, y2):
     distance = math.sqrt(((x1 - x2)**2) + ((y1 - y2)**2))
     return distance
 
-def set_to_merge(df, merge_limit, check_list, merge_set=[], merge_set_list = [], flag = False):
+def set_to_merge(df, merge_limit, check_list, merge_set=[], merge_set_list = []):
+    
+    # Examine every spots for neighboring spots within merge_limit
     while len(check_list) > 0:
         df_x = df.loc[check_list[0], 'x [nm]']
         df_y = df.loc[check_list[0], 'y [nm]']
@@ -24,6 +27,7 @@ def set_to_merge(df, merge_limit, check_list, merge_set=[], merge_set_list = [],
         
         df_search = df_search.loc[df_search.index >= check_list[0]]
         
+        # If there are any other spots within the range, evaluate the distance
         if len(df_search) > 1:
             
             spot1_x = df_search.loc[df_search.index[0], 'x [nm]']
@@ -40,20 +44,70 @@ def set_to_merge(df, merge_limit, check_list, merge_set=[], merge_set_list = [],
                         merge_set = [df_search.index[0], df_search.index[i]]
                     else:
                         merge_set.append(df_search.index[i])
-                    merge_set, merge_set_list = set_to_merge(df, merge_limit, list(range(df_search.index[i], df.shape[0])), merge_set)
-                
-                check_list.remove(df_search.index[i])
-
-            break
+                    
+                    # Search if there is another neighboring spot of the neighboring spot (function within function).
+                    # Since we only need to search for the neighboring spots of one spot (df_search.index[i]),
+                    #   the length of check_list is 1.
+                    merge_set, merge_set_list = set_to_merge(df, merge_limit, [df_search.index[i]], merge_set, merge_set_list)
         
-        check_list.remove(check_list[0])
+        # Make sure this block is not operated in function within function, which we can distinguish by the length of check_list.
+        # When examination of one spot is done, remove it from the check_list, so that we can end the while loop.
+        if len(check_list) != 1:
+            if len(merge_set) > 0:
+                for spot in set(merge_set):
+                    check_list.remove(spot)
+                merge_set_list.append(set(merge_set))
+                merge_set = []
+                
+            else:
+                check_list.remove(check_list[0])
+        else:
+            check_list.remove(check_list[0])
     
     return merge_set, merge_set_list
 
-def merge_multiple_spots_in_one_cell(df, merge_limit):
+def merge_multiple_spots_in_one_cell(df, merge_limit, merge_variable = "intensity/sigma", merge_option=min):
     check_list = list(range(df.shape[0]))
-    merge_set = set_to_merge(df, merge_limit, check_list)
+    merge_set, merge_set_list = set_to_merge(df, merge_limit, check_list)
+    
+    print(f"{len(merge_set_list)} spots with multiple marks")
+    print(f"- Before merge = {df.shape}")
+    
+    # For all the merge_set lists, select one spot that can represent the others
+    #   according to the options you have set.
+    n_merge = 0
+    for spots in merge_set_list:
+        spots = list(spots)
+        compare_list = []
+        for x in spots:
+            if merge_variable == "intensity [nm]" or merge_variable == "sigma [nm]":
+                value = df.loc[x, merge_variable]
+                compare_list.append(value)
+                
+            elif merge_variable == "intensity/sigma":
+                intensity = df.loc[x, 'intensity [photon]']
+                sigma = df.loc[x, 'sigma [nm]']
+                
+                compare_list.append(intensity/sigma)
+                
+        n_merge += (len(compare_list) - 1)
         
+        # Replace x,y position by the average value of the spots to merge
+        avg_x = numpy.mean(df.loc[spots, "x [nm]"])
+        avg_y = numpy.mean(df.loc[spots, "y [nm]"])
+        
+        spot_to_remain = compare_list.index(merge_option(compare_list))
+        
+        df.loc[spots[spot_to_remain], "x [nm]"] = avg_x
+        df.loc[spots[spot_to_remain], "y [nm]"] = avg_y
+        
+        # Drop the rows except the spot to maintain
+        spots.pop(spot_to_remain)
+        df.drop(spots, inplace=True)
+    
+
+    df.reset_index(drop=True, inplace=True)
+    print(f"- After merge = {df.shape}    : {n_merge} spot info removed/merged \n")
     
     return df
 
@@ -68,4 +122,8 @@ if __name__ == "__main__":
     df_C3 = pd.read_csv(base + "C3_Result.csv")
     df_C4 = pd.read_csv(base + "C4_Result.csv")
     
-    merge_multiple_spots_in_one_cell(df_C1, 50)
+    # Three variables and two options to merge spots:
+    #     "sigma [nm]", "intensity [nm]", "intensity/sigma"
+    #     min, max
+    # "intensity/sigma" and "min" as default
+    df_C1 = merge_multiple_spots_in_one_cell(df_C4, 60, "intensity/sigma", min)
--- spot_position_matching_20221125.py
+++ spot_position_matching_20221125.py
@@ -8,6 +8,7 @@ Match spot positions
 import pandas as pd
 import math
 from collections import Counter
+import numpy
 
 
 
@@ -15,17 +16,108 @@ def eucledean (x1, x2, y1, y2):
     distance = math.sqrt(((x1 - x2)**2) + ((y1 - y2)**2))
     return distance
 
-def merge_spots(df, merge_limit):
-    for i in range(0, df.shape[0]):
-        df_x = df.loc[i, 'x [nm]']
-        df_y = df.loc[i, 'y [nm]']
+
+
+def set_to_merge(df, merge_limit, check_list, merge_set=[], merge_set_list = []):
+    
+    # Examine every spots for neighboring spots within merge_limit
+    while len(check_list) > 0:
+        df_x = df.loc[check_list[0], 'x [nm]']
+        df_y = df.loc[check_list[0], 'y [nm]']
         
         # Filter possible spots before calculating distance
         # Reduce number of calculations
         df_search = df[(df['x [nm]'] < (df_x + merge_limit)) & (df['x [nm]'] > (df_x - merge_limit)) & 
                         (df['y [nm]'] < (df_y + merge_limit)) & (df['y [nm]'] > (df_y - merge_limit))]
+        
+        df_search = df_search.loc[df_search.index >= check_list[0]]
+        
+        # If there are any other spots within the range, evaluate the distance
+        if len(df_search) > 1:
+            
+            spot1_x = df_search.loc[df_search.index[0], 'x [nm]']
+            spot1_y = df_search.loc[df_search.index[0], 'y [nm]']
+            
+            for i in range(1, len(df_search)):
+                spot2_x = df_search.loc[df_search.index[i], 'x [nm]']
+                spot2_y = df_search.loc[df_search.index[i], 'y [nm]']
+                
+                distance = eucledean(spot1_x, spot2_x, spot1_y, spot2_y)
+                
+                if distance <= merge_limit:
+                    if merge_set == []:
+                        merge_set = [df_search.index[0], df_search.index[i]]
+                    else:
+                        merge_set.append(df_search.index[i])
+                    
+                    # Search if there is another neighboring spot of the neighboring spot (function within function).
+                    # Since we only need to search for the neighboring spots of one spot (df_search.index[i]),
+                    #   the length of check_list is 1.
+                    merge_set, merge_set_list = set_to_merge(df, merge_limit, [df_search.index[i]], merge_set, merge_set_list)
+        
+        # Make sure this block is not operated in function within function, which we can distinguish by the length of check_list.
+        # When examination of one spot is done, remove it from the check_list, so that we can end the while loop.
+        if len(check_list) != 1:
+            if len(merge_set) > 0:
+                for spot in set(merge_set):
+                    check_list.remove(spot)
+                merge_set_list.append(set(merge_set))
+                merge_set = []
+                
+            else:
+                check_list.remove(check_list[0])
+        else:
+            check_list.remove(check_list[0])
+    
+    return merge_set, merge_set_list
+
+def merge_multiple_spots_in_one_cell(df, merge_limit, merge_variable = "intensity/sigma", merge_option=min):
+    check_list = list(range(df.shape[0]))
+    merge_set, merge_set_list = set_to_merge(df, merge_limit, check_list)
+    
+    print(f"{len(merge_set_list)} spots with multiple marks")
+    print(f"- Before merge = {df.shape}")
+    
+    # For all the merge_set lists, select one spot that can represent the others
+    #   according to the options you have set.
+    n_merge = 0
+    for spots in merge_set_list:
+        spots = list(spots)
+        compare_list = []
+        for x in spots:
+            if merge_variable == "intensity [nm]" or merge_variable == "sigma [nm]":
+                value = df.loc[x, merge_variable]
+                compare_list.append(value)
+                
+            elif merge_variable == "intensity/sigma":
+                intensity = df.loc[x, 'intensity [photon]']
+                sigma = df.loc[x, 'sigma [nm]']
+                
+                compare_list.append(intensity/sigma)
+                
+        n_merge += (len(compare_list) - 1)
+        
+        # Replace x,y position by the average value of the spots to merge
+        avg_x = numpy.mean(df.loc[spots, "x [nm]"])
+        avg_y = numpy.mean(df.loc[spots, "y [nm]"])
+        
+        spot_to_remain = compare_list.index(merge_option(compare_list))
+        
+        df.loc[spots[spot_to_remain], "x [nm]"] = avg_x
+        df.loc[spots[spot_to_remain], "y [nm]"] = avg_y
+        
+        # Drop the rows except the spot to maintain
+        spots.pop(spot_to_remain)
+        df.drop(spots, inplace=True)
+    
+
+    df.reset_index(drop=True, inplace=True)
+    print(f"- After merge = {df.shape}    : {n_merge} spot info removed/merged \n")
     
     return df
+
+
+
 
 def Find_matches(df1, df2):
     match_dic = {}
@@ -268,6 +360,10 @@ def main(df_list, channels, limit):
     
     return df_integrated
 
+
+
+
+
 def fill_missing_values(df_integrated, df_C1, df_C2, df_C3, df_C4):
     # Fill intensity values with average background signal intensity
     bkg_C1 = df_C1['bkgstd [photon]'].mean()
@@ -299,7 +395,18 @@ if __name__ == "__main__":
     df_C3 = pd.read_csv(base + "C3_Result.csv")
     df_C4 = pd.read_csv(base + "C4_Result.csv")
     
-    df_list = [df_C1, df_C2, df_C3, df_C4]
+    '''
+    Three variables and two options to merge spots:
+       - "sigma [nm]", "intensity [nm]", "intensity/sigma"(default)
+       - min(default), max
+    '''
+    merge_limit = 60
+    df_C1 = merge_multiple_spots_in_one_cell(df_C1, merge_limit, "intensity/sigma", min)
+    df_C2 = merge_multiple_spots_in_one_cell(df_C2, merge_limit, "intensity/sigma", min)
+    df_C3 = merge_multiple_spots_in_one_cell(df_C3, merge_limit, "intensity/sigma", min)
+    df_C4 = merge_multiple_spots_in_one_cell(df_C4, merge_limit, "intensity/sigma", min)
+    
+    df_list = [df_C1, df_C2, df_C3, df_C4] # You can define the order of integration here
     channels = ['C1', 'C2', 'C3', 'C4'] # Number of channels must match the length of df_list
     
     limit = 150
